752853ca767dd0733ebb08375c55617c6969ba48
Raynaldi <raysnonci@gmail.com>
2023-07-24 06:54:19 +0700

    wpu make 9 squares

---
+++ v2/wpu-tic-tac-toe/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Square from './components/Square'
+
+function App() {
+  return (
+    <>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </>
+  )
+}
+
+export default App
