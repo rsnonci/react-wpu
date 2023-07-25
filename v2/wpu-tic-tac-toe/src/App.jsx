a04f7d7bd002dc05d75f7e3e9cb1bdf1454b75ed
Raynaldi <raysnonci@gmail.com>
2023-07-25 21:40:39 +0700

    fix X state

--- v2/wpu-tic-tac-toe/src/App.jsx
+++ v2/wpu-tic-tac-toe/src/App.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
 import Square from './components/Square'
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+      const nextSquares = squares.slice()
+      nextSquares[index] = 'X'
+      setSquares(nextSquares)
+  }
+
   return (
     <div className='board'>
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      {squares.map((value, index) => (
+        <Square key={index} value={value} onSquareClick={() => handleClick(index)}/>
+      ))}
     </div>
   )
 }
--- v2/wpu-tic-tac-toe/src/components/Square.jsx
+++ v2/wpu-tic-tac-toe/src/components/Square.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
 
-export default function Square() {
-    const [value, setValue] = useState('');
-
-    function handleClick() {
-        setValue('X');
-    }
-
-    return <button className="square" onClick={handleClick}>{value}</button>
+export default function Square({value, onSquareClick}) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>
 }
