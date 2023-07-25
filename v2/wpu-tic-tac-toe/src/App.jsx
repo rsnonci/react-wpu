9a7f09b216de71e58c4f46f7f7d1d4bf26f68f5d
Raynaldi <raysnonci@gmail.com>
2023-07-25 22:13:27 +0700

    Add Winner

--- v2/wpu-tic-tac-toe/src/App.jsx
+++ v2/wpu-tic-tac-toe/src/App.jsx
@@ -6,8 +6,8 @@ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
-      if (squares[index]) return;
-      
+      if (squares[index] || calculateWinner(squares)) return;
+
       const nextSquares = squares.slice()
       nextSquares[index] = xIsNext ? 'X' : 'O'
 
@@ -15,13 +15,43 @@ function Board() {
       setXIsNext(!xIsNext)
   }
 
+  let status = calculateWinner(squares)? `Winner: ${calculateWinner(squares)}` : `Next player: ${xIsNext ? 'X' : 'O'}`
+
   return (
-    <div className='board'>
-      {squares.map((value, index) => (
-        <Square key={index} value={value} onSquareClick={() => handleClick(index)}/>
-      ))}
-    </div>
+    <>
+      <div className='status'>
+        {status}
+      </div>
+      <div className='board'>
+        {squares.map((value, index) => (
+          <Square key={index} value={value} onSquareClick={() => handleClick(index)}/>
+        ))}
+      </div>
+    </>
   )
 }
 
 export default Board
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (let index = 0; index < lines.length; index++) {
+      const [line1, line2, line3] = lines[index];
+      
+      if (squares[line1] && squares[line1] == squares[line2] && squares[line1] == squares[line3]) {
+        return squares[line1]
+      }
+    }
+
+    return false
+}
