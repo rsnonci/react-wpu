450ced1be58a2257918dc1792e653c66f30fd5f3
Raynaldi <raysnonci@gmail.com>
2023-07-26 22:30:47 +0700

    finish wpu tic tac toe

--- v2/wpu-tic-tac-toe/src/App.jsx
+++ v2/wpu-tic-tac-toe/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import Square from './components/Square'
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
+function Board({xIsNext, squares, onPlay}) {
+  // console.log('square', squares);
+  
   function handleClick(index) {
       if (squares[index] || calculateWinner(squares)) return;
 
       const nextSquares = squares.slice()
       nextSquares[index] = xIsNext ? 'X' : 'O'
 
-      setSquares(nextSquares)
-      setXIsNext(!xIsNext)
+      onPlay(nextSquares)
   }
 
   let status = calculateWinner(squares)? `Winner: ${calculateWinner(squares)}` : `Next player: ${xIsNext ? 'X' : 'O'}`
@@ -31,7 +29,45 @@ function Board() {
   )
 }
 
-export default Board
+function Game() {
+  const [history, setHistory] = useState([(Array(9).fill(null))])
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 ===0
+  const currentSquare = history[currentMove]
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+  }
+
+  const moves = history.map((squares, move) => {
+    const description = move > 0? `Next Move #${move}` : 'Go to start the game'
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+export default Game
 
 function calculateWinner(squares) {
     const lines = [
